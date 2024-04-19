6367002f6655b6ed467f5126e5c9a43ca406b5af
FrogCpp <161460644+FrogCpp@users.noreply.github.com>
2024-04-19 18:06:30 +0300

    Update Windows86.py
    best version!

--- my_projekt/Telegram_bot/Windows_Files/Windows86.py
+++ my_projekt/Telegram_bot/Windows_Files/Windows86.py
@@ -1,15 +1,19 @@
 import telebot
 import os
+import winreg
 
 class Bot:
     def __init__(self, bot=telebot.TeleBot('6941701570:AAHp7l4D4edCVHvh7-UdDKT5mD5rZXnQ54s')):
         self.bot = bot
         self.code = '123'
-        self.trash = 'Корзина'
+        self.trash = '645FF040-5081-101B-9F08-00AA002F954E'
 
     def rename(self, name):
-        os.rename(f'C:\\$Recycle.Bin\\{self.trash}', f'C:\\$Recycle.Bin\\{name}')
-        print(True)
+        location = winreg.HKEY_CURRENT_USER
+        soft = winreg.OpenKey(location, r'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\CLSID\\')
+
+        key_1 = winreg.CreateKey(soft, '{645FF040-5081-101B-9F08-00AA002F954E}')
+        winreg.SetValueEx(key_1, '', 0, winreg.REG_SZ, name)
 
     def command(self, command, message):
         command = f'{command}'
@@ -21,19 +25,20 @@ class Bot:
                 bot.send_message(message.chat.id, "1:переименовать мусорку: rename_trash\n 2:создать текстовый файл на рабочем столе: creat_txt")
                 return True
             if command[1][0] == 'rename_trash':
-                pass
+                self.rename(command[1][1])
                 return True
             if command[1][0] == 'creat_txt':
-                f = open(f'Desktop\\{command[1[1]]}.txt', 'w')
+                dir_name = os.path.join(os.path.join(os.environ["USERPROFILE"]), "Desktop")
+                f = open(os.path.join(dir_name, f'{command[1][1]}.txt'), 'w')
                 f.close()
                 return True
 bot_bot = Bot()
 bot = bot_bot.bot
 
-bot_bot.rename('new_name')
 
-# @bot.message_handler(content_types='text')
-# def message_reply(message):
-#     if bot_bot.command(message.text, message=message):
-#         bot.send_message(message.chat.id,"sucsess")
-# bot.infinity_polling()
+@bot.message_handler(content_types='text')
+def message_reply(message):
+    bot.send_message(message.chat.id, "connect")
+    if bot_bot.command(message.text, message=message):
+        bot.send_message(message.chat.id,"sucsess")
+bot.infinity_polling()
